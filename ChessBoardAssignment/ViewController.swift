1f09636e8790f5803188750ac636b21ab0f7f04d
Ilya Yelagov <yelagov@gmail.com>
2021-02-25 14:44:15 +0300

    Initial Commit

---
+++ ChessBoardAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessBoardAssignment
+//
+//  Created by Ilya Yelagov on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
